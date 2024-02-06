c59d13ba826f89dc4a5b5fdb750fa0f5b14fbd26
trilapagg <trilapagg@gmail.com>
2024-02-07 11:56:33 +1300

    no message

--- hashmaker.py
+++ hashmaker.py
@@ -1,28 +1,43 @@
 import hashlib
 import os
+import json
 
 def calculate_sha256(file_path):
     sha256_hash = hashlib.sha256()
     with open(file_path, "rb") as f:
-        # Read and update hash string value in blocks of 4K
         for byte_block in iter(lambda: f.read(4096), b""):
             sha256_hash.update(byte_block)
     return sha256_hash.hexdigest()
 
-def hash_jar_files_in_directory(directory_path, output_file):
-    with open(output_file, "w") as hashes_file:
-        for root, dirs, files in os.walk(directory_path):
-            for file in files:
-                if file.endswith(".jar"):
-                    file_path = os.path.join(root, file)
-                    sha256_hash = calculate_sha256(file_path)
-                    hashes_file.write(f"{file}:{sha256_hash}\n")
-                    print(f"Processed {file}")
+def generate_jar_file_info(directory_path):
+    jar_files_info = []
+    for root, dirs, files in os.walk(directory_path):
+        for file in files:
+            if file.endswith(".jar"):
+                file_path = os.path.join(root, file)
+                file_info = {
+                    "internalName": file.replace('.jar', ''),
+                    "hash": calculate_sha256(file_path),
+                    "size": os.path.getsize(file_path),
+                    "plugins": [f"com.{file.replace('.jar', '')}.{file.replace('.jar', 'Plugin')}"],
+                    "displayName": "Tril" + file.replace('.jar', '').capitalize(),
+                    "description": "",
+                    "provider": "tril"
+                }
+                jar_files_info.append(file_info)
+    return jar_files_info
 
 # Replace 'trilapagg' with the actual path to your directory
 directory_path = 'trilapagg'
-# Output file
-output_file = 'hashes.txt'
 
-hash_jar_files_in_directory(directory_path, output_file)
-print(f"Hashes have been written to {output_file}")
+jar_files_info = generate_jar_file_info(directory_path)
+
+# Convert the list to JSON format
+json_output = json.dumps(jar_files_info, indent=2)
+
+# Optionally, write to a file
+output_file = 'plugins.json'
+with open(output_file, 'w') as f:
+    f.write(json_output)
+
+print(f"JSON data has been written to {output_file}")
